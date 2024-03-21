b51731cf414a0e6d24591aa8231bb345f9a200c6
v1niii <24722@aeffl.pt>
2024-03-21 19:20:46 +0000

    update 0.2

--- user/index.php
+++ user/index.php
@@ -4,9 +4,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>
-    <?php echo $_POST["username"]; ?>
-  </title>
+  <title>Login success!</title>
+  <link rel="icon" type="image/x-icon" href="/images/cat.png">
   <link rel="stylesheet" href="style.css">
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
@@ -14,10 +13,9 @@
     href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
     rel="stylesheet" />
 </head>
-
 <body>
   <?php
-  echo "<form class='login'>";
+  echo "<form class='info'>";
   if (isset ($_POST["enviar"])) {
     echo "<h1>Login success!</h1>";
     $username = $_POST["username"];
